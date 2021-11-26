035500e2e4f8957a90fc8fa61dde9cfae3f7b92d
Rana <ashikex49@gmail.com>
2021-11-26 01:39:49 -1100

    second commit

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(errorHandler);
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`server connecting on ${port}`);
